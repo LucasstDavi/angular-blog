7a9673f2f1c7a0632bf8874c5f5c85bb69db86db
Lucas Davi <lucasstdavi@gmail.com>
2023-05-04 19:53:45 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "HOMEN DE FERRO ATACA NOVAMENTE",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum congue sem lectus, sed ullamcorper risus tincidunt vitae. Etiam eu facilisis lorem. Maecenas euismod metus ut dolor facilisis tincidunt. Aliquam nec porttitor dolor. Vestibulum eleifend elit id efficitur lobortis. Proin lobortis, nunc ut dapibus fringilla, eros arcu vehicula magna, sit amet auctor metus mauris sed mauris. Phasellus consectetur nulla ut tempor tempus. Nam vel pulvinar odio. Pellentesque non rutrum lacus, ac pretium purus. Ut vitae dui ut ipsum sollicitudin fringilla sit amet in quam.",
+    "photoCover": "https://listasnerds.com.br/wp-content/uploads/2021/12/Homem-de-Ferro_DESTAQUE-850x560.png"
+  },
+  {
+    "id":"2",
+    "title": "Conheça o cara que pula de prédio em prédio",
+    "description": "Aenean molestie sollicitudin ligula eget pellentesque. Ut gravida, velit ut maximus finibus, ipsum turpis cursus nisl, a aliquet metus mauris et nunc. Mauris lobortis, orci sed fermentum laoreet, orci dolor posuere leo, eget consectetur ante leo in magna. Sed a hendrerit enim, nec tristique mauris. Etiam ultrices ante risus, a pretium sapien mattis et. Praesent rhoncus ex pharetra augue semper laoreet. Nulla eget turpis mi. Nam tempus ipsum eros, ac gravida augue consequat eu. Phasellus ac orci faucibus ante aliquam euismod id sodales mauris. Mauris odio sapien, viverra vel imperdiet non, efficitur commodo sem. Sed id tincidunt arcu. Aenean tincidunt accumsan ante, interdum blandit arcu sodales sagittis.lá",
+    "photoCover": "https://uploads.metropoles.com/wp-content/uploads/2021/12/20102920/Homem-Aranha-Sem-volta-para-casa.jpg"
+  },
+  {
+  "id":"3",
+  "title": "homen que se verte de mocergo, conheça",
+  "description": "Sed aliquam vitae tortor ut convallis. Nunc tempor, turpis auctor blandit faucibus, orci purus dictum ipsum, eget venenatis leo elit eu nisl. Aenean ultrices vitae libero id tempus. Ut ac lorem sed lorem rhoncus convallis ac ac lacus. Morbi magna purus, pretium eget nibh vel, auctor dignissim odio. Suspendisse vel est pellentesque, tristique ante dignissim, scelerisque elit. Duis id magna vel tellus venenatis cursus. Phasellus in massa et nunc viverra pulvinar. Cras magna nisl, lacinia ac lorem vel, aliquam cursus justo. Sed eget turpis in metus auctor vehicula.",
+  "photoCover": "https://cdn.vox-cdn.com/thumbor/x2GxU4-nJ5O0ohk_-A-G2WQKqTQ=/0x0:1920x1280/1400x933/filters:focal(807x487:1113x793):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/70560605/jbareham_220228_ecl1072_the_batman_0001.0.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Novo seriado lançado conquista o mundo",
+    "description": "Suspendisse potenti. Nullam a egestas massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas finibus neque ut aliquet ultricies. Sed non aliquet turpis, quis dignissim augue. Cras dapibus semper viverra. Nunc ut leo feugiat, maximus arcu sit amet, vehicula nisl. Nam ac semper urna. Vivamus accumsan justo ante, vel tincidunt velit consectetur vel. ",
+    "photoCover": "https://s2.glbimg.com/J_lG5Uz_pe3RoGYYaz-m2z8a8kE=/0x0:1280x720/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e7c91519bbbb4fadb4e509085746275d/internal_photos/bs/2023/k/m/MWhiB7SAy1QOWeAxDFVA/chaves.jpg"
+  }
+]
